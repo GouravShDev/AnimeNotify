ff7e55014f263012bd0eeeda60982eb6c7139421
GouravShDev <Gourav0Sharma1@gmail.com>
2020-11-20 18:07:28 +0530

    Added Comments and functions

--- AnimeNotify.py
+++ AnimeNotify.py
@@ -9,12 +9,29 @@ from sys import exit
 
 apiPath = "https://api.jikan.moe/v3" 
 
+def loadJson(url):
+    # Using request's get method to get data
+    try:
+        response = get(url)
+    except Exception as e:
+        print("Error: " + str(e))
+        exit(1)
+    if(response.status_code == 200):
+        return loads(response.content)
+    else:
+        print("Error: " + str(response.status_code) + " : "+ response.reason)
+        exit(1)
+
 
 def searchAnime():
+    # Ask User name of Anime
+    # Prepare Url for searching Anime titles
     animeName = input("Enter Name of anime: ")
-    subUrl = "/search/anime?q=%s/Zero&page=1" % animeName
-    print(apiPath + subUrl)
-
+    subUrl = "/search/anime?q=%s/Zero&page=1"%animeName
+    dict = loadJson(apiPath + subUrl)
+    # TODO
+    # Shivanshu
+    #jsonParser(dict)
 
 def getChoice():
     #Method to get user Input
@@ -27,6 +44,8 @@ def getChoice():
 
 
 def menu():
+    # Creates Menu which is shown to the user
+    # And Taking input from user
     while(True):
         print("*******AnimeNotify************")
         print("1. Search Anime ")
@@ -41,7 +60,10 @@ def menu():
             
 
 def main():
+    # calling menu function
     menu()
 
 if __name__ == "__main__":
+    # Call main when this python file is not imported 
+    # to other python file
     main()
