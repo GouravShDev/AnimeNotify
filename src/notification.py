dfb01d3f9e36321f6308b729f46ba73d1b84a921
Shivanshu10 <d0tc0m1@protonmail.com>
2020-12-02 22:04:16 +0530

    added custom notification generator

--- src/notification.py
+++ src/notification.py
@@ -1,24 +1,18 @@
-from pynotifier import Notification
-# create notification
-from os import name, getcwd
-# get name of os, execute shell, get cwd
+from tkinter import Tk, Label, Canvas, Frame, PhotoImage
+# show notification
 
-def notification(notifyTitle, notifyDescription, notifyIcon):
-    # params are notification title, descr, iconname
-    # pass name of icon
-    # select icon type accrdg to os name
-    # urgent notification
-    # tested and completed 
-    notifyIcon=getcwd()+'/'+notifyIcon
-    if (name == 'nt'):
-        notifyIcon+='.ico'
-    else:
-        notifyIcon+='.png'
-    Notification(
-        title=notifyTitle,
-        description=notifyDescription,
-        icon_path=notifyIcon, # On Windows .ico is required, on Linux - .png
-        duration=5,                              # Duration in seconds
-        urgency=Notification.URGENCY_CRITICAL
-    ).send()
-
+def genNotification(title, descr, icon, bg):
+    notice = Tk()
+    notice.title("Notification")
+    notice.configure(background='black')
+    notice.overrideredirect(True)
+    resolution=str(int(notice.winfo_screenwidth()*0.25))+"x"+str(int(notice.winfo_screenheight()*0.20))+"+"+str(int(notice.winfo_screenwidth()*0.74))+"+"+str(int(notice.winfo_screenheight()*0.75))
+    notice.geometry(resolution)
+    bg_image=PhotoImage(file = bg)
+    background_label = Label(notice, image=bg_image)
+    background_label.place(x=0, y=0, relwidth=1)
+    background_label.pack()
+    l=Label(notice, text="hello")
+    l.pack()
+    notice.mainloop()
+genNotification("Anime", "descr..", "link", "icon.png")
